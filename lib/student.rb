a6c616d29b832dd222731ab6399efdf6a7bf2c81
Kathleen Carrel <katiecarrel@gmail.com>
2019-02-09 03:03:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,9 +82,8 @@ class Student
   def self.first_X_students_in_grade_10(x)
     sql = "SELECT * FROM students WHERE students.grade = 10 LIMIT ?"
     
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end 
+    DB[:conn].execute(sql, x)
+ 
   end 
   
   def self.all_students_in_grade_X
